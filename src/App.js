e7bdd0ffce5f9caa033148deeaa1293d02a740bd
unknown <mriduldey94@gmail.com>
2021-03-11 02:41:28 +0530

    tinder footer buttons are added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import './App.css';
+import "./App.css";
 import Header from "./components/header/Header";
 import Cards from "./components/tindercards/Cards";
+import SwipeButtons from "./components/swipebuttons/SwipeButtons";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Cards />
-      {/* swipe buttonss*/}
+      <SwipeButtons />
     </div>
   );
 }
