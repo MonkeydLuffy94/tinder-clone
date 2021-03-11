ffd11f6e445e9fa3d39072bb07ca01cd3e24cb92
unknown <mriduldey94@gmail.com>
2021-03-12 01:19:20 +0530

    base url changed after backend hosted

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8001/",
+  baseURL: "https://md-tinder-clone-backend.herokuapp.com/",
 });
 
 export default instance;
